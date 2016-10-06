861a9f9f716660a64d114d53fa8dab3a8c5efe77
Guo Jingjing <jean.guo@sap.com>
2016-10-06 17:51:43 +0200

    [INTERNAL] Hellow world for 3 views

---
+++ app/openui5/pages/pipelines.controller.js
@@ -0,0 +1,42 @@
+sap.ui.controller("sap.ciconnect.pipelines", {
+	
+	onShowHello: function () {
+		alert("Hello Pipeline!")
+	}
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf sap.ciconnect.pipelines
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf sap.ciconnect.pipelines
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf sap.ciconnect.pipelines
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf sap.ciconnect.pipelines
+*/
+//	onExit: function() {
+//
+//	}
+
+});
