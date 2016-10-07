95ebe6ae5ed52d545787e8d99c26f3b49537f9f5
Guo Jingjing <jean.guo@sap.com>
2016-10-07 17:03:31 +0200

    [INTERNAL] add tyles for summary

--- app/openui5/control/Job.js
+++ app/openui5/control/Job.js
@@ -1,14 +1,20 @@
-/**
- * Job types
- *
- * @enum {string}
- * @public
- */
 sap.ciconnect.control.JobType = {
 	Central: "central service",
 	Local: "local team Jenkins"
 };
 
+sap.ciconnect.control.JobGoal = {
+	Build: "Build",
+	Validate: "Validate",
+	Deploy: "Deploy",
+	Other: "Other"
+};
+
+sap.ciconnect.control.JobStatus = {
+	Waiting: "Waiting",
+	Processing: "Processing",
+	Finished: "Finished"
+};
 
 sap.ui.define([
 	"sap/ui/core/Control"
@@ -16,7 +22,9 @@ sap.ui.define([
 	var Job = Control.extend("sap.ciconnect.control.Job", {
 		metadata: {
 			properties: {
-				type: {type: "string", defaultValue: sap.ciconnect.control.JobType.Central}
+				type: {type: "string", defaultValue: sap.ciconnect.control.JobType.Central},
+				goal: {type: "string", defaultValue: sap.ciconnect.control.JobGoal.Build},
+				status: {type: "string", defaultValue: sap.ciconnect.control.JobStatus.Waiting}
 			}
 		}
 	});
--- app/openui5/view/pipelines.controller.js
+++ app/openui5/view/pipelines.controller.js
@@ -1,42 +1,11 @@
-sap.ui.controller("sap.ciconnect.view.pipelines", {
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"], function (Controller, JSONModel) {
+	"use strict";
 	
-	onShowHello: function () {
-		alert("Hello Pipeline!")
-	}
-
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf sap.ciconnect.pipelines
-*/
-//	onInit: function() {
-//
-//	},
-
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf sap.ciconnect.pipelines
-*/
-//	onBeforeRendering: function() {
-//
-//	},
-
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf sap.ciconnect.pipelines
-*/
-//	onAfterRendering: function() {
-//
-//	},
-
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf sap.ciconnect.pipelines
-*/
-//	onExit: function() {
-//
-//	}
-
+	return Controller.extend("sap.ciconnect.view.pipelines", {
+		onInit: function() {
+			var oModel = new JSONModel();
+			oModel.loadData("../data/pipeline.json");
+			this.getView().setModel(oModel);
+		}
+	});
 });
