c0c91357533902b6667ad437fcf6554417e37c22
Guo Jingjing <jean.guo@sap.com>
2016-10-14 09:44:07 +0200

    [INTERNAl] navigation

--- app/openui5/view/summary.controller.js
+++ app/openui5/view/summary.controller.js
@@ -2,7 +2,15 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"], fun
 	"use strict";
 	
 	return Controller.extend("sap.ciconnect.view.summary", {
-		onInit: function() {
+		onPress: function(oEvent) {
+			switch(oEvent.getSource().getHeader()){
+			case "Pipeline Report":
+				this.getView().getModel("setting").setProperty("/selectedTabKey", "Pipelines");
+				break;
+			case "Repository Report":
+				this.getView().getModel("setting").setProperty("/selectedTabKey", "Repositories");
+				break;
+			}
 			
 		}
 	});
