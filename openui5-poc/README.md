a3328d05ecfca850c05c39e9380c714f0e2d595a
vagrant <vagrant@vagrant>
2016-10-21 09:43:18 +0000

    [INTAERNAL] adjust folder structure

---
+++ openui5-poc/README.md
@@ -0,0 +1,13 @@
+Steps to set up openui5 PoC
+--------------
+Prerequisite: nodeJS is installed
+
+ * Download dependencies
+```sh
+npm install
+```
+
+ * Run an http server
+```sh
+node server.js
+```
---
+++ openui5-poc/server.js
@@ -0,0 +1,5 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(9876, function(){
+    console.log('Access by http://localhost:9876 ...');
+});
