492b772989aeec75a49f695b2a2c33dd1d2b65d3
vagrant <vagrant@vagrant>
2016-10-24 15:16:47 +0000

    [INTERNAL] more loader to be loaded for css/jsx

--- react-poc/src/index.js
+++ react-poc/src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
-//import './index.css';
+import App from './App';
+// import './index.css';
 
 ReactDOM.render(
-  <div className="App">
-        <div className="App-header">
-          <h2>Welcome Jean to Webpack+babel+React World!</h2>
-        </div>
-        <p className="App-intro">
-          Start now!
-        </p>
-      </div>,
+  <App />,
   document.getElementById('root')
 );
