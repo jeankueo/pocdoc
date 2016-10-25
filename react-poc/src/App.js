30050c3785a3b262bd0c9550db8247d8c5f3b1aa
vagrant <vagrant@vagrant>
2016-10-25 08:09:28 +0000

    [INTERNAL] include css and svg files. Include one bo control

--- react-poc/src/App.js
+++ react-poc/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-// import {PipelinePage} from '../bower_components/blueocean-plugin/blueocean-dashboard/src/main/js/components/PipelinePage';
-//import PipelineRowItem from '../bower_components/blueocean-plugin/blueocean-dashboard/src/main/js/components/PipelineRowItem.jsx';
+import logo from './logo.svg';
+import './App.css';
+// import {PipelinePage} from '../bower_components/blueocean-plugin/blueocean-dashboard/src/main/js/components/PipelinePage.jsx';
+import PipelineRowItem from '../bower_components/blueocean-plugin/blueocean-dashboard/src/main/js/components/PipelineRowItem.jsx';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<div className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
 					<h2>Welcome Jean to Webpack+babel+React World!</h2>
 				</div>
 				<p className="App-intro">
