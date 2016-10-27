750cd6aaeb5677bf6085205eb38e2d6c3ec6b774
vagrant <vagrant@vagrant>
2016-10-27 08:16:42 +0000

    [INTERNAL] first apperance of pipelinegraph

--- react-poc/src/PipelinePoC.js
+++ react-poc/src/PipelinePoC.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
-import PipelineGraph from '../node_modules/@jenkins-cd/design-language/dist/js/components/PipelineGraph.js';
+import {PipelineGraph, defaultLayout} from '../node_modules/@jenkins-cd/design-language/dist/js/components/PipelineGraph.js';
 
+import {StatusIndicator} from '../node_modules/@jenkins-cd/design-language/dist/js/components/';
+
+const validResultValues = StatusIndicator.validResultValues;
+let __id = 1;
+
+function makeNode(name, children = [], state = validResultValues.not_built, completePercent) {
+	completePercent = completePercent || ((state == validResultValues.running) ? Math.floor(Math.random() * 60 + 20) : 50);
+	const id = __id++;
+	return {name, children, state, completePercent, id};
+}
 
 class PipelinePoC extends Component {
 	render() {
-		return (<p>placeholder</p>);
+		this.state = {layout: defaultLayout};
+		
+		const stages = [
+			makeNode("Success", [], validResultValues.success),
+			makeNode("Failure", [], validResultValues.failure),
+			makeNode("Running", [], validResultValues.running),
+			makeNode("Slow", [], validResultValues.running, 150),
+			makeNode("Queued", [], validResultValues.queued),
+			makeNode("Unstable", [], validResultValues.unstable),
+			makeNode("Aborted", [], validResultValues.aborted),
+			makeNode("Not Built", [], validResultValues.not_built),
+			makeNode("Bad data", [], "this is not my office")
+		];
+
+		// Reduce spacing just to make this graph smaller
+		const layout = { nodeSpacingH: 90 };
+		
+		return (
+			<PipelineGraph stages={stages} layout={layout} />
+		);
 	}
 }
 
