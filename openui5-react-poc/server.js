900b7cb0fedf3fcd46689a0c843f3c8a7fa63cf5
vagrant <vagrant@vagrant>
2016-11-07 10:57:58 +0000

    [INTERNAL] set up mix project and write control wwrapper

--- openui5-react-poc/server.js
+++ openui5-react-poc/server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname + "/..")).listen(7654, function(){
-    console.log('Access by http://localhost:7654/mix-es5-poc/public ...');
+    console.log('Access by http://localhost:7654/openui5-react-poc/public ...');
 });
--- openui5-react-poc/src/control/BOControl.js
+++ openui5-react-poc/src/control/BOControl.js
@@ -1,13 +1,21 @@
-sap.ui.define(["sap.ui.Control"], function (Control) {
+sap.ui.define([
+	"sap.ui.Control"
+], function (Control) {
 	"use strict";
 	
-	var BOControl = Control.extend("", {
+	var BOControl = Control.extend("sap.ciconnect.control.BOControl", {
 		metadata: {
 			properties: {
-				controlName: {type: "string"}
+				controlName: {type: "string"},
+				status: {type: any}
 			}
 		}
 	});
+	
+	BOControl.prototype.setControlName = function (sControlName) {
+		this.setProperty("controlName", sControlName);
+		this._oControlExport = jQuery.sap.require("jenkins.bo." + sControlname);
+	};
 	
 	return BOControl;
 }, true);
