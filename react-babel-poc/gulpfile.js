42530942da364f373ffbef664dc4c9c11c5e9e55
vagrant <vagrant@vagrant>
2016-11-07 08:54:09 +0000

    [INTERNAL] replace space by gulp task.

--- react-babel-poc/gulpfile.js
+++ react-babel-poc/gulpfile.js
@@ -12,24 +12,24 @@ const copy = require('gulp-copy');
 const config = {
 	clean: ["dist"],
 	react: {
-        sources: "bower_components/jenkins-design-language/src/js/components/**/*.{js,jsx}",
-        dest: "dist/bo/js",
-        babel: {
-            presets: ['es2015', 'react', 'stage-0'],
-            plugins: ['transform-es2015-modules-amd']
-        }
-    },
-    less: {
-        sources: "bower_components/jenkins-design-language/less/theme.less",
-        watch: "less/**/*.less", // Watch includes as well as main
-        dest: "dist/bo/assets/css"
-    },
-     copy: {
-        icons: {
-            sources: "bower_components/jenkins-design-language/icons/**/*",
-            dest: "dist/bo/assets/icons"
-        }
-    }
+		sources: "bower_components/jenkins-design-language/src/js/components/**/*.{js,jsx}",
+		dest: "dist/bo/js",
+		babel: {
+			presets: ['es2015', 'react', 'stage-0'],
+			plugins: ['transform-es2015-modules-amd']
+		}
+	},
+	less: {
+		sources: "bower_components/jenkins-design-language/less/theme.less",
+		watch: "less/**/*.less", // Watch includes as well as main
+		dest: "dist/bo/assets/css"
+	},
+	copy: {
+		icons: {
+			sources: "bower_components/jenkins-design-language/icons/**/*",
+			dest: "dist/bo/assets/icons"
+		}
+	}
 };
 
 gulp.task('default', ()=>{
@@ -37,29 +37,34 @@ gulp.task('default', ()=>{
 });
 
 gulp.task("clean", () => 
-    gulp.src(config.clean, {read: false})
-        .pipe(clean()));
+	gulp.src(config.clean, {read: false})
+		.pipe(clean()));
 
 gulp.task("build", ["compile-react", "less", "copy"]);
 
 gulp.task("compile-react", () =>
-    gulp.src(config.react.sources)
-        .pipe(sourcemaps.init())
-        .pipe(babel(config.react.babel))
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest(config.react.dest)));
+	gulp.src(config.react.sources)
+		.pipe(sourcemaps.init())
+		.pipe(babel(config.react.babel))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest(config.react.dest)));
 
 gulp.task("less", () =>
-    gulp.src(config.less.sources)
-        .pipe(sourcemaps.init())
-        .pipe(less())
-        .pipe(rename("jenkins-design-language.css"))
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest(config.less.dest)));
+	gulp.src(config.less.sources)
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(rename("jenkins-design-language.css"))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest(config.less.dest)));
 
 gulp.task("copy", ["copy-icons"/*, "copy-octicons", "copy-normalize", "copy-fontsCSS", "copy-fonts",
-    "copy-componentDocFiles", "copy-licenses-octicons", "copy-licenses-ofl"*/]);
+	"copy-componentDocFiles", "copy-licenses-octicons", "copy-licenses-ofl"*/]);
 
 gulp.task("copy-icons", () =>
-    gulp.src(config.copy.icons.sources)
-        .pipe(copy(config.copy.icons.dest, {prefix: 3})));
+	gulp.src(config.copy.icons.sources)
+		//.pipe(copy(config.copy.icons.dest, {prefix: 3}))
+		.pipe(rename(function (path) {
+			path.basename = path.basename.replace(' ', '%20');
+			console.log(path.basename);
+		}))
+		.pipe(gulp.dest(config.copy.icons.dest)));
