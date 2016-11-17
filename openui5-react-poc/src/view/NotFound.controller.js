37d97f305c7e7eea161e94b26442a0d6f38ff900
Guo Jingjing <jean.guo@sap.com>
2016-11-17 10:43:57 +0100

    [INTERNAL] routing and NotFound page

---
+++ openui5-react-poc/src/view/NotFound.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller"
+], function (Controller) {
+   "use strict";
+   return Controller.extend("sap.ciconnect.view.NotFound", {
+      onInit: function () {
+      }
+   });
+});
