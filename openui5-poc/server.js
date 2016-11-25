712c1eb356565f330b8a3eeccfc6178c5a53e331
Guo Jingjing <jean.guo@sap.com>
2016-11-25 10:15:33 +0100

    [INTERNAL] re-org master

--- openui5-poc/server.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + "/..")).listen(9876, function(){
-    console.log('Access by http://localhost:9876/openui5-poc/public ...');
-});
